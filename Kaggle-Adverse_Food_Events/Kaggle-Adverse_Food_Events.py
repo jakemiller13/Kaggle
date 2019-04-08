e55e0c163ad890a9b803951d964b2b72dca0a095
Jake <jakemiller13@hotmail.com>
2019-04-08 05:51:18 -0400

    Added dataset website

--- Kaggle-Adverse_Food_Events/Kaggle-Adverse_Food_Events.py
+++ Kaggle-Adverse_Food_Events/Kaggle-Adverse_Food_Events.py
@@ -6,6 +6,8 @@ Created on Wed Apr  3 20:35:03 2019
 @author: Jake
 """
 
+# https://www.kaggle.com/fda/adverse-food-events
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
