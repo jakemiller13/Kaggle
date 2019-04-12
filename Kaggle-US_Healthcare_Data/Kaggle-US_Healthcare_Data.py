da1e27d72284cb5df95fe1d86b33a1db04716bdf
Jacob Miller <jmiller@xlrn.com>
2019-04-12 17:13:40 -0400

    Added RandomForestClassifier and GradientBoostingClassifer

--- Kaggle-US_Healthcare_Data/Kaggle-US_Healthcare_Data.py
+++ Kaggle-US_Healthcare_Data/Kaggle-US_Healthcare_Data.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 # Load in dataframe
 df = pd.read_csv('Nutritions_US.csv', encoding = 'cp1252')
@@ -62,7 +62,7 @@ X_test = pd.DataFrame(imp_mean.fit_transform(X_test), columns = features)
 
 print('\n--- Training Decision Tree Classifiers ---')
 tree_correct = []
-for i in range(1, 41):
+for i in range(1, 25):
     tree_clf = DecisionTreeClassifier(max_depth = i)
     tree_clf.fit(X_train, y_train)
     tree_predictions = tree_clf.predict(X_test)
@@ -71,28 +71,69 @@ for i in range(1, 41):
                         len(y_test))
 
 # Plot correct predictions for Decision Tree Classifier
-plt.plot(range(1, 41), tree_correct, 0, 100)
+plt.plot(range(1, 25), tree_correct, 0, 100)
 plt.xlabel('Depth of Decision Tree')
 plt.ylabel('Correct Predictions (%)')
 plt.title('Decision Tree Classifier')
 plt.show()
 
 # Train Random Forest Classifier
-randf_correct = []
+combined_randf = {}
+num_trees = [10, 25, 50, 100, 500]
 print('\n--- Training Random Forest Classifiers ---')
-for i in range(1, 41):
-    randf_clf = RandomForestClassifier(n_estimators = 100,
-                                       max_depth = i,
-                                       random_state = 42)
-    randf_clf.fit(X_train, y_train)
-    randf_predictions = randf_clf.predict(X_test)
-    randf_correct.append(100 *
-                         (randf_predictions == y_test).value_counts()[1] /
-                         len(y_test))
+for trees in num_trees:
+    randf_correct = []
+    print('\n--- Training with [{}] trees ---'.format(trees))
+    for i in range(1, 25):
+        randf_clf = RandomForestClassifier(n_estimators = trees,
+                                           max_depth = i,
+                                           random_state = 42)
+        randf_clf.fit(X_train, y_train)
+        randf_predictions = randf_clf.predict(X_test)
+        randf_correct.append(100 *
+                             (randf_predictions == y_test).value_counts()[1] /
+                             len(y_test))
+    combined_randf[trees] = randf_correct
 
 # Plot correct predictions for Random Forest Classifier
-plt.plot(range(1, 41), randf_correct, 0, 100)
-plt.xlabel('Depth of Decision Tree Used')
+labels = []
+for i in combined_randf.keys():
+    plt.plot(range(1, 25), combined_randf[i])
+    labels.append(i)
+plt.xlabel('Depth of Decision Trees Used')
 plt.ylabel('Correct Predictions (%)')
-plt.title('Random Forest Classifier - 100 Trees Used')
+plt.ylim(0, 100)
+plt.xlim(0, 30)
+plt.title('Random Forest Classifier')
+plt.legend(labels)
+plt.show()
+
+# Train Gradient Boosted Classifier Trees
+combined_grbc = {}
+print('\n--- Training Gradient Boosting Classifier ---')
+for trees in num_trees:
+    grbc_correct = []
+    print('\n--- Training GRBC with [{}] trees ---'.format(trees))
+    for i in range(1, 25):
+        grbc_clf = GradientBoostingClassifier(n_estimators = trees,
+                                              max_depth = i,
+                                              random_state = 42)
+        grbc_clf.fit(X_train, y_train)
+        grbc_predictions = grbc_clf.predict(X_test)
+        grbc_correct.append(100 *
+                            (grbc_predictions == y_test).value_counts()[1] /
+                            len(y_test))
+    combined_grbc[trees] = grbc_correct
+
+# Plot correct predictions for Gradient Boosting Classifier
+grbc_labels = []
+for i in combined_grbc.keys():
+    plt.plot(range(1, 25), combined_grbc[i])
+    grbc_labels.append(i)
+plt.xlabel('Depth of Decision Trees Used')
+plt.ylabel('Correct Predictions (%)')
+plt.ylim(0, 100)
+plt.xlim(0, 30)
+plt.title('Gradient Boosting Classifier')
+plt.legend(grbc_labels)
 plt.show()
