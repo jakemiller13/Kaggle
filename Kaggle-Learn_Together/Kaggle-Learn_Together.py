e96068ac6686b5a8e9c2939f18e3e03576c7da20
Jacob Miller <jmiller@xlrn.com>
2019-09-06 12:57:36 -0400

    Just starting out

--- Kaggle-Learn_Together/Kaggle-Learn_Together.py
+++ Kaggle-Learn_Together/Kaggle-Learn_Together.py
@@ -5,5 +5,11 @@ Created on Fri Sep  6 12:36:47 2019
 @author: jmiller
 """
 
+# Imports
 import pandas as pd
 import numpy as np
+
+# Load data and data description description
+df = pd.read_csv('train.csv')
+with open('Data_Description.txt') as file:
+    desc = file.read()
