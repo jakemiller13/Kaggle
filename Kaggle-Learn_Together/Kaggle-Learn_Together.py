79781c07a0f49c56cf5e948465d7d8c29b7f610d
Jacob Miller <jmiller@xlrn.com>
2019-09-06 15:23:21 -0400

    Very basic beginnings during free time at work

--- Kaggle-Learn_Together/Kaggle-Learn_Together.py
+++ Kaggle-Learn_Together/Kaggle-Learn_Together.py
@@ -8,8 +8,14 @@ Created on Fri Sep  6 12:36:47 2019
 # Imports
 import pandas as pd
 import numpy as np
+from sklearn import train_test_split
 
 # Load data and data description description
 df = pd.read_csv('train.csv')
 with open('Data_Description.txt') as file:
     desc = file.read()
+
+
+
+X = df[df.columns[:-1]]
+y = df[df.columns[-1]]
